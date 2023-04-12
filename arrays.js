03545361559ed21aa83fe7ace34d3bc9ac230836
Habibou Moussa <habibou997@gmail.com>
2023-04-12 11:43:26 +0000

    Access Multi-Dimensional Arrays With Indexes

--- arrays.js
+++ arrays.js
@@ -1,10 +1,22 @@
-// 1
+// 1- strore many values in a variable using array
 var my_array = ["habibou", 1, "Moussa", "Daouda"];
 
-// 2
-var multi_arry = [["Habibou", 23], ["Moussa"], 34, 45];
+// 2) Nest arrays within annother array
+var multi_arry = [
+  ["Habibou", 23],
+  ["Moussa"],
+  34,
+  45,
+  [1, 2, 3, ["Habibou", 23]],
+  [4, 5, 6],
+  [7, 8, 9],
+  [[10, 11, 12], 13, 14],
+];
 
-//3
+//3)Access Array Data with Indexes
 const data = my_array[0];
-//4
+//4) Modify Array Data With Indexes
 my_array[0] = "Adamu";
+
+// 5) Access Multi-Dimensional Arrays With Indexes
+multi_arry[3][3][0];
